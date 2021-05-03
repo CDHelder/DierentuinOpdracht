df9619d9c39a047349f937690ffa5dd9ac978654
C Helder <christian.helder@itvitaelearning.nl>
2021-05-03 11:42:16 +0200

    Add project files.

---
+++ DierentuinOpdracht.Common/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DierentuinOpdracht.Common
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Energy { get; set; }
+
+        public void Eat()
+        {
+            Energy += 25;
+        }
+    }
+}
